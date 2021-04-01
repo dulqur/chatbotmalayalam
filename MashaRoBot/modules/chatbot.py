d7efa7684cc017c4bd8aa8c63063065c73e8873a
peterX <74089102+zyrus-basi@users.noreply.github.com>
2021-04-01 14:06:19 +0530

    Update chatbot.py

--- MashaRoBot/modules/chatbot.py
+++ MashaRoBot/modules/chatbot.py
@@ -142,13 +142,9 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
-*Commands:* 
-*Admins only:*
- ❍ /addchat*:* Enables Chatbot mode in the chat.
- ❍ /rmchat*:* Disables Chatbot mode in the chat.
-Reports bugs at @{SUPPORT_CHAT}
-[Powered by CoffeeHouse](https://coffeehouse.intellivoid.net) from @Intellivoid
+*ENEBLE XANDREA IN YOUR GROUP* 
+  /addchat*:* Enables lydia mode in the chat.
+  /rmchat*:* Disables lydia mode in the chat.
 """
 
 ADD_CHAT_HANDLER = CommandHandler("addchat", add_chat)
